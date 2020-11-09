72ba7f844b66d2d9094a03f3669e280c728e2333
Richard See <Richard_See@discovery.com>
2020-11-09 23:41:23 +0000

    Returning JSON

--- functions/boop.js
+++ functions/boop.js
@@ -1,5 +1,6 @@
 // Export a function called handler
 // Async so you do not have to fire a callback, just return a value
+// Using common.js syntax
 exports.handler = async () => {
   return {
     statusCode: 200,

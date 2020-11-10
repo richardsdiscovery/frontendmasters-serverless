f664b984051b83522f5048534c2737460ffccb35
Richard See <Richard_See@discovery.com>
2020-11-10 17:21:33 +0000

    Fetching Data from the OMDb API

--- functions/movies.js
+++ functions/movies.js
@@ -3,8 +3,33 @@ const fetch = require('node-fetch');
 const movies = require('../data/movies.json');
 
 exports.handler = async () => {
+  const api = new URL('https://www.omdbapi.com');
+
+  // Add the secret API key to the query string
+  api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+
+  // All movies execute at the same time. Then resolve all promises with Promise.all
+  // https://www.learnwithjason.dev/blog/keep-async-await-from-blocking-execution/
+  const promises = movies.map((movie) => {
+    // Use the movie's IMDB ID to look up details
+    api.searchParams.set('i', movie.id); // Set the movie id as "i"
+
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+
+        return {
+          ...movie,
+          scores
+        };
+      });
+  });
+
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies)
+    body: JSON.stringify(moviesWithRatings)
   };
 };

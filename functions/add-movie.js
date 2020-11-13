0a3590075f4fb44a7feca7163c8ac8e8487e85a8
Richard See <Richard_See@discovery.com>
2020-11-13 16:59:48 +0000

    Adding to the Database

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,11 +1,24 @@
 const { query } = require('./util/hasura');
 
 exports.handler = async (event) => {
-  console.log('HELLO!', event)
+  const { id, title, tagline, poster } = JSON.parse(event.body);
+
+  const result = await query({
+    query: `
+      mutation ($id: String!, $title: String!, $tagline: String!, $poster: String!) {
+        insert_movies_one(object: { id: $id, title: $title, tagline: $tagline, poster: $poster }) {
+          id,
+          poster,
+          tagline,
+          title
+        }
+      }
+    `,
+    variables: { id, title, tagline, poster }
+  });
 
   return {
     statusCode: 200,
-    body: 'wip'
+    body: JSON.stringify(result)
   }
-  // const { id, title, tagline, poster } = JSON.parse(event.body);
 }
